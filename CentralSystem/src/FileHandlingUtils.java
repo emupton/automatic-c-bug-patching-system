83ba429b0f98c11e566505c46bf6cbbee61d6456
hiketra <emma.upton.95@gmail.com>
2016-02-03 18:29:42 +0000

    New file handling class
    Utility class for file handling. Currently has a method to overwrite a
    line in a specific file in an output file… look at relevant method to
    gain a clearer idea

---
+++ CentralSystem/src/FileHandlingUtils.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+/**
+ * File handling utility class, contains various static methods used for
+ * manipulating the input and output .c files
+ * 
+ * @author emma
+ *
+ */
+public class FileHandlingUtils {
+	/**
+	 * Creates a duplicated version of 'input', however, at line 'lineNumber' 'input''s
+	 * line is over-written with 'lineReplacement' in the 'output' file.
+	 * 
+	 * @param input
+	 * @param output
+	 * @param lineReplacement
+	 * @param lineNumber
+	 * @return
+	 * @throws IOException
+	 */
+	public static boolean writeLineAt(File input, File output, String lineReplacement, int lineNumber) throws IOException{
+		FileWriter fw = new FileWriter(output);
+		int lineNo = 0;
+		try (BufferedReader br = new BufferedReader(new FileReader(input))){
+			String line="";
+			while((line = br.readLine())!=null){
+				String newLine;
+				if(lineNo == lineNumber-1){
+					newLine = lineReplacement;
+				}
+				else{
+					newLine = line;
+				}
+				fw.write(newLine +"\n");
+				lineNo++;
+			}
+		}
+		fw.close();
+		return false;
+	}
+}
