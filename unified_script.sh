ae12f82a68c85d78b6b7b425fc422699f6a5a8c6
hiketra <emma.upton.95@gmail.com>
2016-02-03 16:01:20 +0000

    Added comments, removed some debug-specific and redundant code

--- unified_script.sh
+++ unified_script.sh
@@ -1,18 +1,25 @@
-PROBLEM_FILE=$1
-MISC_DIR=$2
-OUTPUT_FILE=$3
-PARSER="/Users/Emma/Desktop/Projects/Parser/src/Driver"
-JSOUP="/Users/emma/Desktop/Projects/jsoup-1.8.3.jar"
-CENTRAL_SYSTEM='/Users/Emma/Desktop/Projects/CentralSystem'
-/Applications/checker-277/scan-build -o $MISC_DIR gcc -c $PROBLEM_FILE
+PROBLEM_FILE=$1 #String file directory of the problem .c file
+MISC_DIR=$2 #Misc directory for files generated during the bug fixing process,
+#user should provide a directory they want to use for these files to be #generated in
+OUTPUT_FILE=$3 #The output file directory
+PARSER="java -jar /users/emma/desktop/parser.jar " #Location of the parser.jar file
+CENTRAL_SYSTEM='/Users/Emma/Desktop/Projects/CentralSystem/src' #Location of the central system's directory
+/Applications/checker-277/scan-build -o $MISC_DIR gcc -c $PROBLEM_FILE #Calling scan-build to detect bugs and output the file report .html files to MISC_DIR
 cd $MISC_DIR
 x=$(ls -t $MISC_DIR | head -n 1)
+#getting the first directory that will contain releant problem report files, including the .html
 cd $x
 y=`ls -d report*`
-echo "This is y: " $y
+#obtaining the report.html file (report files are of the format xreport.html, where x is an arbritrary string of numbers)
 REPORT="$MISC_DIR/$x/$y"
-echo "This is the final directory" $REPORT
-PARSED_OUTPUT=`java -jar /users/emma/desktop/parser.jar "$REPORT"`
+#storing the report file location
+echo "Scan-build's report is stored at: " $REPORT
+PARSED_OUTPUT=`$PARSER "$REPORT"`
+#parsing the report file, will output the bug description and line numbers
 echo $PARSED_OUTPUT
-cd /Users/Emma/Desktop/Projects/CentralSystem/src
+#displaying the parsed output for dedbugging purposes
+cd $CENTRAL_SYSTEM
+#changing directory to the location of the central system
 java -cp .:/Users/emma/downloads/antlr-4.5.1-complete.jar Driver "$PARSED_OUTPUT" "$PROBLEM_FILE" "$OUTPUT_FILE"
+#sending the parsed output along with the problem file and output file directories to the central system
+#the central system then fixes the bug(s) and outputs a patched file
