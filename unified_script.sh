33dc7d03d415dc4658f64b1d889d14544b8eca3f
hiketra <emma.upton.95@gmail.com>
2016-02-04 22:10:01 +0000

    Changed central sys local location

--- unified_script.sh
+++ unified_script.sh
@@ -3,7 +3,7 @@ MISC_DIR=$2 #Misc directory for files generated during the bug fixing process,
 #user should provide a directory they want to use for these files to be #generated in
 OUTPUT_FILE=$3 #The output file directory
 PARSER="java -jar /users/emma/desktop/parser.jar " #Location of the parser.jar file
-CENTRAL_SYSTEM='/Users/Emma/Desktop/Projects/CentralSystem/src' #Location of the central system's directory
+CENTRAL_SYSTEM='/Users/emma/Desktop/Projects/FinalYear/fixBugAntlr/CentralSystem/src' #Location of the central system's directory
 /Applications/checker-277/scan-build -o $MISC_DIR gcc -c $PROBLEM_FILE #Calling scan-build to detect bugs and output the file report .html files to MISC_DIR
 cd $MISC_DIR
 x=$(ls -t $MISC_DIR | head -n 1)
